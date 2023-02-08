445fb40f14a7d111b835cd937f904e02da12127f
jayykal <92835027+jayykal@users.noreply.github.com>
2023-02-07 21:16:06 -0500

    navbar skeleton

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
 import "./index.css"
+import {Routes, Route, Navigate} from 'react-router-dom'
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div>
-      <button className="bg-red-500">Text</button>
-      <h1>TEXT</h1>
-    </div>
+    <Routes>
+      <Route exact path='/' element={<Home/>}/>
+      <Route path='*' element={<Navigate to='/'/>}/>
+    </Routes>
+    // <div>
+    //   <button className="bg-red-500">Text</button>
+    //   <h1>TEXT</h1>
+    // </div>
   );
 }
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,16 @@
-
+import './navbar.css'
 
 export default function Navbar() {
   return (
-    <div>
-      
+    <div className='navbarContainer'>
+      <div className='navbarLeft'>
+      </div>
+      <div className='navbarCenter'>
+        <h1 className='navbarTitle'>Soundcheck!</h1>
+      </div>
+      <div className='navbarRight'>
+        <button className='profileBtn'>Profile</button>
+      </div>
     </div>
   )
 }
