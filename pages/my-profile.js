b6f2423ba7bdbf2f8a6476afb3a93e3c064d656c
bsbriansongbs <43419494+bsbriansongbs@users.noreply.github.com>
2023-03-09 19:31:32 -0500

    profile page being worked on

--- pages/my-profile.js
+++ pages/my-profile.js
@@ -1,20 +1,30 @@
 import { Flex } from "@mantine/core";
+import { Image } from '@mantine/core';
 import { useSession, getSession } from "next-auth/react";
 
 function MyProfile() {
   const { data: session } = useSession();
-
+  console.log(typeof session?.user?.image);
+  var img_link = "" + session?.user?.image;
+  session?.user?.image
+  console.log(session);
   return (
     <Flex
       justify={"center"}
       align={"center"}
-      direction={"column"}
+      direction={"row"}
+      wrap={"wrap"}
       h={"calc(100vh - 5rem)"}
       style={{
         transform: "translateY(5rem)",
       }}
     >
-      {JSON.stringify(session, null, 2)}
+      <Flex>
+        {JSON.stringify(session?.user?.name)}
+      </Flex>
+      <Flex>
+       <Image width={240} height={240} mx = "auto" radius="md" src={img_link} fit="contain" alt="Random image" />
+      </Flex>
     </Flex>
   );
 }
--- pages/profile/[profile].js
+++ pages/profile/[profile].js
@@ -1,5 +1,13 @@
+import {Image} from '@mantine/core';
+
 function Profile() {
-  return <div>Profile</div>;
+  var img_link = "" + session?.user?.image;
+  console.log(img_link);
+  return( 
+  <Image width={240} mx = "auto" radius="md" src={img_link} alt="Random image" />
+  
+
+  );
 }
 
 export default Profile;
